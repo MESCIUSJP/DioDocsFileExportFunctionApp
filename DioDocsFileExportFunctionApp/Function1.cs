59d603750aa76850f14b768447f5d2961c5c5228
Kuni <kuni.ujiie@grapecity.com>
2021-06-08 14:17:11 +0900

    プロジェクト ファイルを追加します。

---
+++ DioDocsFileExportFunctionApp/Function1.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using GrapeCity.Documents.Excel;
+
+namespace DioDocsFileExportFunctionApp
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string Message = string.IsNullOrEmpty(name)
+                ? "Hello, World!!"
+                : $"Hello, {name}!!";
+
+            //Workbook.SetLicenseKey("");
+
+            Workbook workbook = new Workbook();
+            workbook.Worksheets[0].Range["A1"].Value = Message;
+
+            byte[] output;
+
+            using (var ms = new MemoryStream())
+            {
+                workbook.Save(ms, SaveFileFormat.Xlsx);
+                output = ms.ToArray();
+            }
+
+            return new FileContentResult(output, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            {
+                FileDownloadName = "Result.xlsx"
+            };
+        }
+    }
+}
---
+++ DioDocsFileExportFunctionApp/Function2.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using GrapeCity.Documents.Pdf;
+using GrapeCity.Documents.Text;
+using System.Drawing;
+
+namespace DioDocsFileExportFunctionApp
+{
+    public static class Function2
+    {
+        [FunctionName("Function2")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string Message = string.IsNullOrEmpty(name)
+                ? "Hello, World!!"
+                : $"Hello, {name}!!";
+
+            //GcPdfDocument.SetLicenseKey("");
+
+            GcPdfDocument doc = new GcPdfDocument();
+            GcPdfGraphics g = doc.NewPage().Graphics;
+
+            g.DrawString(Message,
+                new TextFormat() { Font = StandardFonts.Helvetica, FontSize = 12 },
+                new PointF(72, 72));
+
+            byte[] output;
+
+            using (var ms = new MemoryStream())
+            {
+                doc.Save(ms, false);
+                output = ms.ToArray();
+            }
+
+            return new FileContentResult(output, "application/pdf")
+            {
+                FileDownloadName = "Result.pdf"
+            };
+        }
+    }
+}
