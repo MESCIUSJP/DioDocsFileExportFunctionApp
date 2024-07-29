5682b28744e84b1df9d593ff6f6c5634fae8453a
Kuni <kuni.ujiie@mescius.com>
2024-07-29 13:39:04 +0900

    各ライブラリを更新

--- DioDocsFileExportFunctionApp/Function1.cs
+++ DioDocsFileExportFunctionApp/Function1.cs
@@ -1,13 +1,12 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using GrapeCity.Documents.Excel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using GrapeCity.Documents.Excel;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DioDocsFileExportFunctionApp
 {
